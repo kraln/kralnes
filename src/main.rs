b0af74deca7adead3276f87f2a34905cb38c7a33
Jeff Katz <jeff.katz@kiwi.ki>
2017-08-05 22:24:12 +0200

    Further improvement
    Fixed up some of the types...

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::fs::File;
+use std::io::Read;
+
 #[macro_use]
 extern crate sciter;
 
 use sciter::{HELEMENT, Element, Value};
 
-const GFX_WIDTH: u8 = 255;
-const GFX_HEIGHT: u8 = 239;
+const GFX_WIDTH: usize = 256;
+const GFX_HEIGHT: usize = 240;
 
 static mut inc: u8 = 0;
 fn main()
@@ -29,21 +32,21 @@ impl Handler
     fn get_gfx_buffer(&mut self) -> Value
     {
 
-        let mut gfx_buffer = [[0; GFX_WIDTH as usize]; GFX_HEIGHT as usize];
+        let mut gfx_buffer = [[0; GFX_WIDTH]; GFX_HEIGHT];
 
         // unroll gfx_buffer into the appropriate format
-        let mut buf = [0u8; ((GFX_WIDTH as usize)*(GFX_HEIGHT as usize)*4+12)];
+        let mut buf = [0u8; ((GFX_WIDTH)*(GFX_HEIGHT)*4+12)];
 
         // this is a sciter format for images. header. bgra data follows
         // the A will always be 0...
         buf[0] = 'B' as u8; buf[1] = 'G' as u8;  buf[2] = 'R' as u8; buf[3] = 'A' as u8;
-        buf[4] = 0;   buf[5] = 0;    buf[6] = 0;   buf[7] = GFX_WIDTH;
-        buf[8] = 0;   buf[9] = 0;   buf[10] = 0;  buf[11] = GFX_HEIGHT;
+        buf[4] = 0;   buf[5] = 0;    buf[6] = ((GFX_WIDTH & 0xff00) >> 8) as u8;   buf[7] = (GFX_WIDTH & 0xff) as u8;
+        buf[8] = 0;   buf[9] = 0;   buf[10] = ((GFX_HEIGHT & 0xff00) >> 8) as u8;  buf[11] = (GFX_HEIGHT & 0xff) as u8;
 
         let mut idx: usize = 12;
-        for y in 0..GFX_HEIGHT as usize
+        for y in 0..GFX_HEIGHT
         {
-            for x in 0..GFX_WIDTH as usize
+            for x in 0..GFX_WIDTH
             {
                 let col = gfx_buffer[y][x];
                 unsafe {
@@ -52,24 +55,29 @@ impl Handler
                 buf[idx+2] = inc;// ((col | 0b11111111000000000000000000000000) >> 24 & 0xff) as u8; // b
                 buf[idx+3] = inc;// 255; // a
                 inc = inc.wrapping_add(1);
+                if(inc > 254) {inc = 0};
                 }
                 idx+=4;
             }
         }
-
-        let res: Value = Value::from(&buf[..]);
-        res
+        Value::from(&buf[..])
     }
 
     fn tick(&mut self, ) -> Value
     {
         // CPU run a frame
-        let mut res = Value::new();
+        Value::new()
+    }
 
-
-        // stuff
-
-        res
+    fn load_rom(&mut self, mut filename: String) -> Value
+    {
+        let mut data = Vec::new();
+        println!("Asked to open file {:?}", filename);
+        let actual_filename = filename.split_off(7);
+        let mut f = File::open(actual_filename).expect("Unable to open file");
+        f.read_to_end(&mut data).expect("Unable to read data");
+        println!("{}", data.len());
+        Value::from(&data[..])
     }
 }
 
@@ -85,6 +93,7 @@ impl sciter::EventHandler for Handler
     {
         fn tick();
         fn get_gfx_buffer();
+        fn load_rom(String);
     }
 }
 
